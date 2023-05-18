607ae002196fbc143cc07205676b6f99365c2214
daisuke <daisuke.0208.k@icloud.com>
2023-05-19 00:24:59 +0900

    Modularization of 'Food and Dish binding function'

---
+++ app/lib/link_food_to_dish.rb
@@ -0,0 +1,16 @@
+module LinkFoodToDish
+    def linkFoodToDish(main, arr, link, errors)
+        action = link == 'Dish' ? true : false
+        arr.each do |ele|
+            existEle = action ? Dish.find_by(name: ele) : Food.find_by(name: ele)
+            if existEle && action
+                DishesFood.find_or_create_by(dish: existEle, food: main)
+            elsif existEle && !action
+                DishesFood.find_or_create_by(dish: main, food: existEle)
+            else
+                errors << "#{link} '#{ele}' not found."
+                raise ActiveRecord::RecordInvalid.new(DishesFood.new), errors.join(", ")
+            end
+        end
+    end
+end
